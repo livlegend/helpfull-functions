60195b2fbeba0709d5f32ea45a3d9fbc0a74dd16
livlegend <livsenyo@gmail.com>
2022-01-05 20:33:53 +0000

    add scroll function

--- functions.js
+++ functions.js
@@ -5,3 +5,11 @@ function check(given_string) {
     if(regex.test(given_string)) return true ;
     return false;
 }
+
+// SCROLL TO SPECIFIC SECTION ON PAGE AFTER PAGE LOADED (Jquery)
+function scroll(){
+    // Handler for .ready() called.
+    $('html, body').animate({
+        scrollTop: $('#what').offset().top
+    }, 'slow'); 
+}
